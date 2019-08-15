1a7776c26cfdb1cb98a67961b3ef9e1a154d23dc
ThariduJayaratne <34066349+ThariduJayaratne@users.noreply.github.com>
2019-08-15 15:32:38 +0400

    file change

--- contactform.php
+++ contactform.php
@@ -9,6 +9,6 @@ if(isset($_POST['submit'])){
   $headers = "From: ".$mailFrom;
   $txt = "You have received an email from ".$name.".\n\n".$message;
   mail($mailTo, $subject, $txt, $headers);
-  header("location: contact.php?mailsend");
+  header("location: index.php?mailsend");
 }
 ?>
