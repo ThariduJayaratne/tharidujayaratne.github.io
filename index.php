51aaafa063da2d64d94ba4f0e34f3585f56bc95d
ThariduJayaratne <34066349+ThariduJayaratne@users.noreply.github.com>
2019-08-15 15:38:00 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Tharidu Jayaratne</title>
+    <link rel="stylesheet" type="text/css" href="css/style4.css">
+  </head>
+<body>
+  <!-- <div class="contact-title">
+    <h2>Feel free to contact me</h2>
+  </div>
+  <div class="contact-form">
+    <form class="contact-form" method="post" action="contactform.php">
+      <input  style="font-family: Montserrat, sans-serif; color:#fff;"  name="name" type="text" class="form-control" placeholder="Your Name" required>
+      <br>
+      <input  style="font-family: Montserrat, sans-serif; color:white;" name="mail" type="email" class="form-control" placeholder="Your Email" required><br>
+      <textarea style="font-family: Montserrat, sans-serif; color:white;" name="message" class="form-control" placeholder="Message" row="4" required></textarea><br>
+      <input  style="font-family: Montserrat, sans-serif; color:white;" type="submit" name="submit" class="form-control submit" value="Submit Message">
+</form>
+</div> -->
+<main>
+   <div class="contact-title">
+  <h2>Feel free to contact me</h2>
+</div>
+<div class="contact-form">
+    <form class="contact-form" action="contactform.php" method="post">
+      <input  style="font-family: Montserrat, sans-serif; color:#fff;" class="form-control" type="text" name-"name" placeholder="Full name" required><br>
+      <input  style="font-family: Montserrat, sans-serif; color:#fff;" class="form-control" type="text" name="mail" placeholder="Your e-mail" required><br>
+      <textarea  style="font-family: Montserrat, sans-serif; color:#fff;" class="form-control" name="message" placeholder="Message"></textarea><br>
+      <button style="font-family: Montserrat, sans-serif; color:white;" class="form-control submit" type="submit" name="submit">SUBMIT</button>
+    </form>
+</div>
+</main>
+</body>
+</html>
